14a54218bc7649ea6f85e6ce2d1ea194c96cbd8f
Gregory Davis <davisgreg1@gmail.com>
2018-02-19 17:51:08 -0500

    added the initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       This gonna be a fuckin great ass game.
+      </div>
+    );
+  }
+}
+
+export default App;
