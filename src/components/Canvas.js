977a273d4d0931591d2de118466c2cc399490c2b
Gregory Davis <davisgreg1@gmail.com>
2018-02-20 09:45:06 -0500

    added static cannonball and static currentscore

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,27 +4,45 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
 
 const Canvas = (props) => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-  return (
-    <svg
-      id="aliens-go-home-canvas"
-      preserveAspectRatio="xMaxYMax none"
-      onMouseMove={props.trackMouse}
-      viewBox={viewBox}
-    >
-      <Sky />
-      <Ground />
-      <CannonPipe rotation={props.angle} />
-      <CannonBase />
-    </svg>
-  );
+    const viewBox = [
+        window.innerWidth / -2,
+        100 - window.innerHeight,
+        window.innerWidth,
+        window.innerHeight
+    ];
+    return (
+
+        <svg
+            id="aliens-go-home-canvas"
+            preserveAspectRatio="xMaxYMax none"
+            onMouseMove={props.trackMouse}
+            viewBox={viewBox}>
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow dx="1" dy="1" stdDeviation="2"/>
+                </filter>
+            </defs>
+            <Sky/>
+            <Ground/>
+            <CannonPipe rotation={props.angle}/>
+            <CannonBase/>
+            <CannonBall
+                position={{
+                x: 0,
+                y: -100
+            }}/>
+            <CurrentScore score={15}/>
+        </svg>
+    );
 };
 
 Canvas.propTypes = {
-  angle: PropTypes.number.isRequired,
-  trackMouse: PropTypes.func.isRequired,
+    angle: PropTypes.number.isRequired,
+    trackMouse: PropTypes.func.isRequired
 };
 
 export default Canvas;
